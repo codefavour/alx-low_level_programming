cbd507461a0a11162ccfde406d5e08bd16400ed0
codefavour <119820284+codefavour@users.noreply.github.com>
2022-12-10 17:15:16 -0800

    completed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * DESCRIPTION: 'the program's description'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
